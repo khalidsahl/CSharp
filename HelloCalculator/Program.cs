535cc29c61a6a3c751d76ff671b1ba3a27309d48
Khalid Sahlqeath <finlux89@outlook.com>
2016-12-18 16:15:46 +0100

    Inserted breakpoints with conditions

--- HelloCalculator/Program.cs
+++ HelloCalculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloCalculator
 {
@@ -10,6 +6,17 @@ namespace HelloCalculator
     {
         static void Main(string[] args)
         {
+            int result = AddNumbers(64, 32);
+            Console.WriteLine(result);
+            Console.WriteLine(AddNumbers(-1, 32));
+            Console.ReadKey();
+        }
+
+        private static int AddNumbers(int numberOne, int numberTwo)
+        {
+            int CalculationResult;
+            CalculationResult = numberOne + numberTwo;
+            return CalculationResult;
         }
     }
 }
