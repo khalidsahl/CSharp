14e372c1ab3b9f86d3ac97af027404677a357ad0
Khalid Sahlqeath <finlux89@outlook.com>
2016-12-20 18:43:19 +0100

    Made calculator

---
+++ CalculatorConsole/Arthimetic.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculatorConsole
+{
+    public class Arthimetic
+    {
+        public double Division(double x, double y)
+        {
+            return x / y; 
+        }
+
+        public double Multiplication(double x, double y)
+        {
+            return x*y;
+        }
+
+        public double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
